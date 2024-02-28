d85d3dede941e067e27879f6cf6bcdec8f5d4498
Dimas Saputra <dimas.maulanads@gmail.com>
2024-02-28 12:14:20 +0700

    add utils.js

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+function indexOfMaxNumber(numbers) {
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return -1;
+  }
+
+  const reducer = (max, current, currentIndex) => (current > max.value
+    ? { value: current, index: currentIndex }
+    : max);
+
+  const { index } = numbers.reduce(reducer, { value: numbers[0], index: 0 });
+
+  return index;
+}
+
+export { indexOfMaxNumber };
